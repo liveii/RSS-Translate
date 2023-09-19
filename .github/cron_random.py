1023aed90cd874795506fa0b5f1c3626d1d94a1a
Endest <61148158+endest@users.noreply.github.com>
2023-09-19 23:27:49 +0800

    Update cron_random.py

--- .github/cron_random.py
+++ .github/cron_random.py
@@ -8,7 +8,7 @@ YML=".github/workflows/circle_translate.yml"
 
 f = open(YML, "r+", encoding="UTF-8")
 list1 = f.readlines()           
-list1[7] = "   - cron: '%d %d * * *'\n"%(min,h)
+list1[7] = "   - cron: '0 */2 * * *'\n"
 
 f = open(YML, "w+", encoding="UTF-8")
 f.writelines(list1)
